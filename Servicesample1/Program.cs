7cfd1e0a19326ce0dc6efaba37712214e16da9c4
Harsh Patel <harsh3608@gmail.com>
2023-06-07 13:14:53 +0530

    completed worker service

--- Servicesample1/Program.cs
+++ Servicesample1/Program.cs
@@ -1,10 +1,22 @@
-using Servicesample1;
+using App.WindowsService;
+using Microsoft.Extensions.Logging.Configuration;
+using Microsoft.Extensions.Logging.EventLog;
 
-IHost host = Host.CreateDefaultBuilder(args)
-    .ConfigureServices(services =>
-    {
-        services.AddHostedService<Worker>();
-    })
-    .Build();
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddWindowsService(options =>
+{
+    options.ServiceName = ".NET Joke Service";
+});
 
+LoggerProviderOptions.RegisterProviderOptions<
+    EventLogSettings, EventLogLoggerProvider>(builder.Services);
+
+builder.Services.AddSingleton<JokeService>();
+builder.Services.AddHostedService<WindowsBackgroundService>();
+
+// See: https://github.com/dotnet/runtime/issues/47303
+builder.Logging.AddConfiguration(
+    builder.Configuration.GetSection("Logging"));
+
+IHost host = builder.Build();
 host.Run();
