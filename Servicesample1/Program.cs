a03b295d49f8d7bf806133a72ddffce27f33ab39
Harsh Patel <harsh3608@gmail.com>
2023-06-07 13:03:23 +0530

    Add project files.

---
+++ Servicesample1/Program.cs
@@ -0,0 +1,10 @@
+using Servicesample1;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
